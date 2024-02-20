ceb1ab04df8fcf10849265a914d73728ee04b845
ThanichaMongkorn <thanica_mongkorn@cmu.ac.th>
2024-02-20 19:43:34 +0700

    edit frontend for count pepople online

--- websocket-demo-frontend/src/store/Slices/webSocketSlice.ts
+++ websocket-demo-frontend/src/store/Slices/webSocketSlice.ts
@@ -12,6 +12,7 @@ interface webSocketMessage {
     content: string;
     timestamp: string;
     type: messageType;
+    onlineUser: number;
 }
 interface webSocketState {
     isConnected: boolean;
